fe0b4a0538cc3ea8bc48a51bfc54e742a94c1760
ErkinOZ <68461447+ErkinOZ@users.noreply.github.com>
2021-01-25 01:52:20 +0600

    Add files via upload

---
+++ FlutterVerstka/widgets/top_bar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget {
+  TopBar({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: Image.network(
+                  'https://www.edigitalagency.com.au/wp-content/uploads/afterpay-logo-png-black-transparent.png')
+              .image,
+          fit: BoxFit.contain,
+        ),
+        gradient: LinearGradient(
+          colors: [
+            Colors.white,
+            Colors.black,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 3),
+                  child: IconButton(
+                    icon: Icon(Icons.dehaze),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 3),
+                  child: IconButton(
+                    icon: Icon(Icons.calendar_today),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 50,
+                  width: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      //!-
+                    ],
+                  ),
+                ),
+                Chip(
+                  backgroundColor: Color.fromRGBO(255, 0, 0, 5),
+                  label: Text(
+                    '02.06.2020',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 5),
+                  child: IconButton(
+                    icon: Icon(Icons.insert_chart),
+                    color: Colors.white,
+                    tooltip: 'Показать график',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
